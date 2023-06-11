2b78ac8d0d66d8978aead189e4044d2750837a5f
Konrad Romański <konrad.romanski4@gmail.com>
2023-06-11 13:15:02 +0200

    Added TypeORM and PostgreSQL

--- blockchain-backend/src/app.module.ts
+++ blockchain-backend/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlockchainModule } from './blockchain/blockchain.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const ormconfig = require('../ormconfig.json');
 
 @Module({
-  imports: [BlockchainModule],
+  imports: [TypeOrmModule.forRoot(ormconfig), BlockchainModule],
   controllers: [AppController],
   providers: [AppService],
 })
